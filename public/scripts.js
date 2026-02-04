f88cb15d622f1c4ffa79dc7b6a4376c1ab8e82d6
Kitkatisvibing <kate.jadi@gmail.com>
2026-02-04 13:11:26 +0100

    all aside animations done closes #2

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,24 @@
-let leftButton = document.querySelector('.blinkies')
+let leftButton = document.querySelector('.blinkieButton')
 leftButton.addEventListener('click', slideRight)
 function slideRight() {
     leftButton.classList.toggle('leftMoved')
+    socialButton.classList.remove('rightMoved')
+    socialButton.classList.remove('rightMoved')
 }
+
+let socialButton = document.querySelector('.socialButton')
+socialButton.addEventListener('click', slideLeft)
+function slideLeft() {
+    socialButton.classList.toggle('rightMoved')
+    leftButton.classList.remove('leftMoved')
+    stampButton.classList.remove('rightMoved')
+}
+
+let stampButton = document.querySelector('.stampButton')
+stampButton.addEventListener('click', leftSlide)
+function leftSlide() {
+    stampButton.classList.toggle('rightMoved')
+    leftButton.classList.remove('leftMoved')
+    socialButton.classList.remove('rightMoved')
+}
+
